eba4c420a5fa8925e46458f2c379645457409d42
Robert Thurnreiter <7965327+thr27@users.noreply.github.com>
2025-01-13 12:49:46 +0100

    Mini Server prod

--- plays/tasks/fti/files/certbot/renew-ssl.sh
+++ plays/tasks/fti/files/certbot/renew-ssl.sh
@@ -6,7 +6,7 @@ echo $0 $*
 
 source /etc/haproxy/certbot/.env
 
-certbot certonly ${IS_STAGING} --webroot -w ${WEB_ROOT} ${DOMAIN_LIST} \
+certbot certonly ${IS_STAGING} --expand --webroot -w ${WEB_ROOT} ${DOMAIN_LIST} \
     --email ${EMAIL} \
     --agree-tos --non-interactive --agree-tos --no-eff-email --no-redirect \
     --deploy-hook ${SCRIPTPATH}/deploy-ssl.sh
