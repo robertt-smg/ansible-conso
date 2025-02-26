26a839a5b68a45f25c61e2822fd72b3e7cc43cf2
Robert Thurnreiter <7965327+thr27@users.noreply.github.com>
2025-02-26 14:19:03 +0100

    WIP

---
+++ smg-conso.vm/dbserver1/up.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SCRIPT="$(readlink -f -- $0)"
+SCRIPTPATH="$(dirname $SCRIPT)"
+
+source ${SCRIPTPATH}/../../lib/up.sh $*
+
+CERT_DIR=$(realpath --relative-to="$SCRIPTPATH" "$CERT_DIR")
+VM_NAME=SMG-TST-DB1
+VMName=SMG-TST-DB1
+VMPassword=$DefaultGuestAdminPassword
+VMMacAddress=00155D1ABC31
+VMIpAddress=192.168.121.31
+
+is_windows
+
+function run() {
+    hyper_v_build
+}
+win_sudo_me $0 $*
--- smg-conso.vm/lserver2/up.sh
+++ smg-conso.vm/lserver2/up.sh
@@ -5,11 +5,11 @@ SCRIPTPATH="$(dirname $SCRIPT)"
 source ${SCRIPTPATH}/../../lib/up.sh $*
 
 CERT_DIR=$(realpath --relative-to="$SCRIPTPATH" "$CERT_DIR")
-VM_NAME=SMG-TST-L01
-VMName=SMG-TST-L01
+VM_NAME=SMG-TST-L02
+VMName=SMG-TST-L02
 VMPassword=$DefaultGuestAdminPassword
-VMMacAddress=00155D1ABCAA
-VMIpAddress=192.168.121.20
+VMMacAddress=00155D1ABC22
+VMIpAddress=192.168.121.22
 
 is_windows
 
