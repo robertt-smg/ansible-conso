a21a95d3e47e1ac284acb0b0013ebe88a3f59145
Robert Thurnreiter <7965327+thr27@users.noreply.github.com>
2025-03-31 12:29:28 +0200

    Add Strato.vm

--- strato_stg.vm/server1-old-onpodman/up.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SCRIPT="$(readlink -f -- $0)"
-SCRIPTPATH="$(dirname $SCRIPT)"
-
-source ${SCRIPTPATH}/../../lib/up.sh $*
-
-CERT_DIR=$(realpath --relative-to="$SCRIPTPATH" "$CERT_DIR")
-
-up
