96e0181bb0b77d8418c2c829e02d275f906aa096
Robert Thurnreiter <7965327+thr27@users.noreply.github.com>
2025-04-02 15:46:42 +0200

    Update Strato

--- strato_stg.vm/dbserver1/up.sh
+++ strato_stg.vm/dbserver1/up.sh
@@ -5,8 +5,8 @@ SCRIPTPATH="$(dirname $SCRIPT)"
 source ${SCRIPTPATH}/../../lib/up.sh $*
 
 CERT_DIR=$(realpath --relative-to="$SCRIPTPATH" "$CERT_DIR")
-VM_NAME=SMG-TST-DB1
-VMName=SMG-TST-DB1
+VM_NAME=SMG-STG-DB1
+VMName=SMG-STG-DB1
 VMPassword=$DefaultGuestAdminPassword
 VMMacAddress=00155D1ABC31
 VMIpAddress=172.16.115.31
