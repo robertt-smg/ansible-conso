d3fcd272f5eb306197c4f41d4c559dc6ff595dd6
Robert Thurnreiter <7965327+thr27@users.noreply.github.com>
2025-06-25 09:02:40 +0200

    Timeout / Terminal Server Setup

---
+++ strato_stg.vm/lserver3/up.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SCRIPT="$(readlink -f -- $0)"
+SCRIPTPATH="$(dirname $SCRIPT)"
+
+source ${SCRIPTPATH}/../../lib/up.sh $*
+
+CERT_DIR=$(realpath --relative-to="$SCRIPTPATH" "$CERT_DIR")
+VM_NAME=SMG-STG-L02
+VMName=SMG-STG-L02
+VMPassword=$DefaultGuestAdminPassword
+VMMacAddress=00155D1ABC23
+VMIpAddress=172.16.115.23
+VMImageVersion=24.04
+VMProcessorCount=2
+VMMemory=3GB
+is_windows
+
+function run() {
+    hyper_v_build
+}
+win_sudo_me $0 $*
