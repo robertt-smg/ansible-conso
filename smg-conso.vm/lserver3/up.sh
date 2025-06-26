4660ffc3861b77af8eb8371cf10e1335e99d21b4
Robert Thurnreiter <7965327+thr27@users.noreply.github.com>
2025-06-26 14:55:07 +0200

    Fix Fserver1 / Add Lseerver3 / Add Passbolt FTI

--- smg-conso.vm/lserver3/up.sh
+++ smg-conso.vm/lserver3/up.sh
@@ -5,8 +5,8 @@ SCRIPTPATH="$(dirname $SCRIPT)"
 source ${SCRIPTPATH}/../../lib/up.sh $*
 
 CERT_DIR=$(realpath --relative-to="$SCRIPTPATH" "$CERT_DIR")
-VM_NAME=SMG-TST-L02
-VMName=SMG-TST-L02
+VM_NAME=SMG-TST-L03
+VMName=SMG-TST-L03
 VMPassword=$DefaultGuestAdminPassword
 VMMacAddress=00155D1ABC23
 VMIpAddress=192.168.121.23
